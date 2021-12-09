293dfa52bd758aacb0193d0db8ee6f4d3ba6a578
Apostolos Giannoulidis <32461460+agiannoul@users.noreply.github.com>
2021-12-09 23:26:48 +0200

    Update taks1.scala

--- src/main/scala/assinment/taks1.scala
+++ src/main/scala/assinment/taks1.scala
@@ -66,7 +66,7 @@ object taks1 {
 
     //==================== TEST ==========================================
     val cluster0 = predictions.filter(predictions("prediction") === 0 )
-
+    // TF-IDF
     val hashingTF = new HashingTF().setInputCol("Words").setOutputCol("rRawFeatures") //.setNumFeatures(20000)
     val featurizedDF = hashingTF.transform(cluster0)
 
@@ -74,21 +74,45 @@ object taks1 {
     val idfM = idf.fit(featurizedDF)
     val completeDF = idfM.transform(featurizedDF)
 
-
+    // method to apply on columns
     val udf_Values_Vector = udf( (v: SparseVector) => v.values )
     val udf_Values_TfVector = udf( (v: SparseVector) => v.values.map(x=> x/v.values.size) )
 
     val complete_valuesDF=completeDF.select($"Words",$"member_name",$"features",$"prediction",udf_Values_TfVector($"rRawFeatures").as("tf_value"),udf_Values_Vector($"rFeatures").as("idf_value"))
+    // udf to combine tf and idf values
     val udf_tf_idf = udf( (tf: Array[Double],idf: Array[Double]) => {
       for (i <- 0 to idf.length-1){
           tf(i)=tf(i)*idf(i)
       }
       tf
     } )
+    //udf: calculates the distance from center of cluster
     val distance_from_center =  udf((features: Vector, c: Int) => sqrt(Vectors.sqdist(features, model.clusterCenters(c))))
 
-    val completeTF_IDF_DF=complete_valuesDF.select($"Words",$"member_name",$"features",$"prediction",$"tf_value",$"idf_value",udf_tf_idf($"tf_value",$"idf_value").as("tf_idf_value"),distance_from_center($"features",$"prediction").as("rank"))
+    val completeTF_IDF_DF=complete_valuesDF.select($"Words",$"member_name",$"features",$"prediction",$"tf_value",$"idf_value",udf_tf_idf($"tf_value",$"idf_value").as("tf_idf_value"),distance_from_center($"features",$"prediction").as("dist"))
     completeTF_IDF_DF.show(5)
+
+    // Given a group of speeches extract  most representative keywords.
+
+    // method-1 keep k most significant words per speech. Then keep top n frequent words.
+
+    val most_significant_k =udf((Words: List[String],tfidf: Array[Double]) => {
+      var sign_words = List[String]()
+      val k=5
+      for( i <-0 until min(k,Words.size)){
+        val maxx=tfidf.reduceLeft(_ max _)
+        val indexmax=tfidf.indexOf(maxx)
+        val wordd=Words(indexmax)
+        sign_words =  sign_words ::: List(wordd)
+        tfidf(indexmax) = -1
+      }
+      sign_words
+    })
+    val mswords = completeTF_IDF_DF.select(most_significant_k($"Words",$"idf_value").as("sign_words"))
+    mswords.show(10,false)
+    val rdd =mswords.rdd
+    val rdd0=rdd.map(_(0).asInstanceOf[Seq[String]]).flatMap(x=>x).map(word => (word,1)).reduceByKey(_+_).sortBy(_._1,false)
+    rdd0.top(5).foreach(println)
   }
 
 }

4cc1e1c1c1e6ba1233904e79b5a71f15fb0cbbde
agiannoul <apostolosgiannoulidis@gmail.com>
2021-12-26 19:15:21 +0200

    Make faster Word2Vec and better structure of code

--- src/main/scala/assinment/taks1.scala
+++ src/main/scala/assinment/taks1.scala
@@ -12,38 +12,45 @@ import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.spark.sql.functions.{col, column, udf}
 
 object taks1 {
-  val ss = SparkSession.builder().master("local").appName("assigment").getOrCreate()
+  val ss = SparkSession.builder().master("local[*]").appName("assigment").getOrCreate()
   import ss.implicits._ // For implicit conversions like converting RDDs to DataFrames
 
   def main(args: Array[String]): Unit = {
     // Create the spark session first
     ss.sparkContext.setLogLevel("ERROR")
+    val timeee = time {
+      val inputFile = "./Greek_Parliament_Proceedings_1989_2020_Clean_V2.csv"
+      //val currentDir = System.getProperty("user.dir")  // get the current directory
+      //val outputDir = "file://" + currentDir + "/output"
 
-    val inputFile = "./Greek_Parliament_Proceedings_1989_2020_Clean_V2.csv"
-    //val currentDir = System.getProperty("user.dir")  // get the current directory
-    //val outputDir = "file://" + currentDir + "/output"
+      println("reading from input file: " + inputFile)
 
-    println("reading from input file: " + inputFile)
+      // Read the contents of the csv file in a dataframe. The csv file does not contain a header.
+      val basicDF = ss.read.option("header", "true").csv(inputFile)
+      val sampleDF = basicDF.sample(0.01, 1234)
+      //sample set
+      //val notnulldf = sampleDF.filter(sampleDF("member_name").isNotNull && sampleDF("clean_speech").isNotNull)
+      //ALL set
+      val notnulldf = basicDF.filter(basicDF("member_name").isNotNull && basicDF("clean_speech").isNotNull)
 
-    // Read the contents of the csv file in a dataframe. The csv file does not contain a header.
-    val basicDF = ss.read.option("header", "true").csv(inputFile)
-    val sampleDF = basicDF.sample(0.01, 1234)
-    val notnulldf = sampleDF.filter(sampleDF("member_name").isNotNull && sampleDF("clean_speech").isNotNull)
+      //val keywordsofsampleData=algo_topics(notnulldf, ss)
+      val Seg = 5
+      val udf_segmentTime = udf((v: String) => (v.takeRight(4).toInt - 1989) / Seg)
 
-    //val keywordsofsampleData=algo_topics(notnulldf, ss)
-    val Seg=5
-    val udf_segmentTime = udf((v: String) => (v.takeRight(4).toInt-1989)/Seg)
+      val dfwithseg = notnulldf.select($"member_name", $"sitting_date", $"member_name", $"clean_speech", udf_segmentTime($"sitting_date").as("Segment"))
 
-    val dfwithseg=notnulldf.select($"member_name",$"sitting_date",$"member_name",$"clean_speech",udf_segmentTime($"sitting_date").as("Segment"))
-    dfwithseg.show(5)
-    val loop = new Breaks;
-    loop.breakable {
-      for (seg <- 0 to 4) {
-        val segmenteddf=dfwithseg.filter($"Segment" === seg)
-        val keywordsofsampleData=algo_topics(segmenteddf)
+      //time consuming
+      val dfwordsTovec = worsdToVec(dfwithseg)
+
+      println("ALG0")
+      val loop = new Breaks;
+      loop.breakable {
+        for (seg <- 0 to 4) {
+          val segmenteddf = dfwordsTovec.filter($"Segment" === seg)
+          val keywordsofsampleData = algo_topics(segmenteddf)
+        }
       }
     }
-
 
     //=====================================================================================================
     //                                How topics change across years
@@ -56,23 +63,17 @@ object taks1 {
     // algo_topics(time segment of data)
   }
 
-  def filterstopwords(word: String): Boolean = {
-    word.endsWith("ώ") || word.endsWith("ω") || word.endsWith("ει") ||
-      word.endsWith("γιατί") || word.endsWith("αλλά") || word.endsWith("ότι") || word.endsWith("αυτό") ||
-      word.endsWith("αυτή") || word.endsWith("αυτά") || word.endsWith("εσείς") || word.endsWith("αυτοί")
-  }
 
-
-  def algo_topics(notnulldf: DataFrame): Array[String] = {
-
+  // TODO look for faster/better word2Vec algorithm (tf-idf?)
+  // Add a column of "features" based on column with name "clean_speech"->String
+  // based on predefine algorithm
+  def worsdToVec(notnulldf: DataFrame): DataFrame ={
     val udf_clean = udf((s: String) => s.replaceAll("""([\p{Punct}&&[^.]]|\b\p{IsLetter}{1,2}\b)\s*""", ""))
 
-    val newDF = notnulldf.select($"member_name", udf_clean($"clean_speech").as("cleaner")).persist()
+    val newDF = notnulldf.withColumn("cleaner",udf_clean(col("clean_speech"))).persist()
 
     val tokenizer = new Tokenizer().setInputCol("cleaner").setOutputCol("Words")
     val wordsDF = tokenizer.transform(newDF)
-
-    //wordsDF.show(5)
     /*val hashingTF = new HashingTF().setInputCol("Words").setOutputCol("rRawFeatures") //.setNumFeatures(20000)
     val featurizedDF = hashingTF.transform(wordsDF)
     featurizedDF.printSchema()
@@ -88,17 +89,26 @@ object taks1 {
       .setOutputCol("result")
       .setVectorSize(50)
       .setMinCount(0)
+      .setNumPartitions(10)
     val modelw = word2Vec.fit(wordsDF)
     val resultDf = modelw.transform(wordsDF)
 
 
 
     val featureDf = resultDf.withColumnRenamed("result", "features")
+    featureDf
+  }
+
+  //Cluster features with k-means
+  //calculate tf,idf
+  //keep values from sparse vector and calculate keywords (method2keywords) in each cluster
+  def algo_topics(featureDf: DataFrame): Array[String] = {
+
+
     val kmeans = new KMeans().setK(5).setSeed(1L).setMaxIter(100)
     val model = kmeans.fit(featureDf)
 
     val predictions = model.transform(featureDf)
-    model.clusterCenters.foreach(println)
     //======================= OK ========================================
 
     //==================== TEST ==========================================
@@ -126,9 +136,8 @@ object taks1 {
     //udf: calculates the distance from center of cluster
     val distance_from_center = udf((features: Vector, c: Int) => sqrt(Vectors.sqdist(features, model.clusterCenters(c))))
 
-    val completeTF_IDF_DF = complete_valuesDF.select($"Words", $"member_name", $"features", $"prediction", $"tf_value", $"idf_value", udf_tf_idf($"tf_value", $"idf_value").as("tf_idf_value"), distance_from_center($"features", $"prediction").as("dist"))
-    completeTF_IDF_DF.show(5)
-
+    val  completeTF_IDF_DF = complete_valuesDF.select($"Words", $"member_name", $"features", $"prediction", $"tf_value", $"idf_value", udf_tf_idf($"tf_value", $"idf_value").as("tf_idf_value"), distance_from_center($"features", $"prediction").as("dist"))
+    val completeTF_IDF_DF_Non_Empty = completeTF_IDF_DF.filter(Row => Row.get(0).asInstanceOf[Seq[String]].nonEmpty)
     // Given a group of speeches extract  most representative keywords.
 
 
@@ -136,7 +145,7 @@ object taks1 {
     // method-2
 
 
-    val keywords = method2keywords(completeTF_IDF_DF, 40, 1)
+    val keywords = method2keywords(completeTF_IDF_DF_Non_Empty, 40, 1)
     keywords
 
   }
@@ -156,14 +165,13 @@ object taks1 {
     })
 
     val mswords = completeTF_IDF_DF.select(most_significant_k($"Words", $"tf_idf_value").as("sign_words"))
-    mswords.show(10, false)
     val rdd = mswords.rdd
     val rdd0 = rdd.map(_ (0).asInstanceOf[Seq[String]]).flatMap(x => x).map(word => (word, 1)).reduceByKey(_ + _).map(x => (x._2, x._1)) //.sortByKey(false)
     rdd0.filter(x => !filterstopwords(x._2)).top(n).foreach(println)
     rdd0.top(n).map(x => x._2)
   }
 
-
+  // keep most significant words based on tfidf and distance from the center of cluster.
   def method2keywords(completeTF_IDF_DF: DataFrame, n: Int, k: Int): Array[String] = {
     val most_significant_k = udf((Words: List[String], tfidf: Array[Double]) => {
       var sign_words = List[String]()
@@ -187,17 +195,40 @@ object taks1 {
         sign_tfidf = sign_tfidf ::: List(maxx)
         tfidf(indexmax) = -1
       }
+
       sign_tfidf
+
+
     })
 
     val mswords = completeTF_IDF_DF.select(most_significant_k($"Words", $"tf_idf_value").as("sign_words"), $"dist", most_significant_k_tfidf($"Words", $"tf_idf_value").as("sign_tf_idf"))
     val rdd = mswords.rdd
+    //score=tf_idf/dist
     val rdd0 = rdd.map(row => (row(0).asInstanceOf[Seq[String]], row(1).asInstanceOf[Double], row(2).asInstanceOf[Seq[Double]])).map(x => (x._1.head, x._3.head / x._2)).reduceByKey(max).map(x => (x._2, x._1))
+
+    // Reporte: write about score, which one is better ? (kanena)
+
+    //score=tf_idf
+    //val rdd0 = rdd.map(row => (row(0).asInstanceOf[Seq[String]], row(1).asInstanceOf[Double], row(2).asInstanceOf[Seq[Double]])).map(x => (x._1.head, x._3.head)).reduceByKey(max).map(x => (x._2, x._1))
     // val rdd0 = rdd.map(row => (row(0).asInstanceOf[Seq[String]], row(1).asInstanceOf[Double])).map(x => (x._1.head, x._2)).reduceByKey(min).map(x => (x._2, x._1)).sortByKey(true)
+
+
     rdd0.filter(x => !filterstopwords(x._2)).top(n).foreach(println)
     //rdd0.filter(x => !filterstopwords(x._2)).take(n).foreach(println)
     rdd0.top(n).map(x => x._2)
     //rdd0.take(n).map(x=>x._2)
+  }
+  def filterstopwords(word: String): Boolean = {
+    word.endsWith("ώ") || word.endsWith("ω") || word.endsWith("ει") ||
+      word.endsWith("γιατί") || word.endsWith("αλλά") || word.endsWith("ότι") || word.endsWith("αυτό") ||
+      word.endsWith("αυτή") || word.endsWith("αυτά") || word.endsWith("εσείς") || word.endsWith("αυτοί")
+  }
+  def time[R](block: => R): R = {
+    val t0 = System.nanoTime()
+    val result = block    // call-by-name
+    val t1 = System.nanoTime()
+    println("Elapsed time: " + (t1 - t0) + "ns")
+    result
   }
 
 }
